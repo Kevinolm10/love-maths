7ebe12ee3a78a80319eb5d91e415d98ec1c97fa2
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-07-17 14:26:06 +0000

    add code to calculate random numbers for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`"You clicked" ${gameType}`);
+            }
+        })
+
+    }
+})
+
+
+
 function runGame() {
 
 }
@@ -26,6 +44,6 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplyQuestion()[
+function displayMultiplyQuestion() {
 
-]
+}
